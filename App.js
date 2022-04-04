e6f1a2ca7b705976616bc9a3ffe1ab80663c9509
jamvit <ot12341@gmail.com>
2022-04-04 17:32:57 -0600

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={[styles.welcome, { flex: 5, backgroundColor: "pink" }]}>
+        Hello react native!
+      </Text>
+      <Text style={[styles.welcome, { flex: 4, backgroundColor: "yellow" }]}>
+        Hello react native!
+      </Text>
+      <Text style={[styles.welcome, { flex: 3, backgroundColor: "red" }]}>
+        Hello react native!
+      </Text>
+      <Text style={[styles.welcome, { flex: 2, backgroundColor: "blue" }]}>
+        Hello react native!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "orange",
+    justifyContent: "space-around",
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
   },
 });
